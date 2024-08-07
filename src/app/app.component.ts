09379eca41c3ca54127d0cccfc4f040dd6550feb
abhisha <abhishasharma004@gmail.com>
2024-08-07 22:49:05 +0545

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-form';
+  emailValidate="/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/"
+  registerForm = new FormGroup({
+    email: new FormControl("",[Validators.required,Validators.maxLength(32),Validators.pattern(this.emailValidate)]),
+    password: new FormControl("",[Validators.required,Validators.maxLength(32),Validators.minLength(8)]),
+    confirm_password: new FormControl("",[Validators.required,Validators.maxLength(32),Validators.minLength(8)])
+
+  })
+  
+  getControl(name:any):AbstractControl|null{
+    return this.registerForm.get(name)
+  }
+  toRegister(){
+  localStorage.setItem("formvalue",JSON.stringify(this.registerForm.value))
+
+  
+    console.log(this.registerForm.value);
+    this.clearInputs()
+    
+  }
+  clearInputs(){
+  this.registerForm.reset();
+  }
+ 
 }
